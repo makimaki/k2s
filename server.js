405774050ac3f01db96a02284e1cbed52019109d
Yuta Sakamaki <yuta_sakamaki@albert2005.co.jp>
2016-02-25 12:17:51 +0900

    公開記事のみを通知

--- server.js
+++ server.js
@@ -48,33 +48,36 @@ var response = {
 
 app.post('/knowledge', function (req, res) {
     console.log('event: ' + JSON.stringify(req.body, null, 4));
+
     var payload = req.body;
 
-    // Slack への投稿フォーマット指定
-    // cf. https://api.slack.com/incoming-webhooks
-    response.attachments = [
-        {
-            "pretext": process.env.KNOWLEDGE_MAN + 'の悪知恵が' + ((payload.status === 'created') ? '増えました' : '更新されました'),
-            "title": payload.title,
-            "title_link": payload.link,
-            "text": payload.content,
-            "fields": [
-                {
-                    "title": "登録者",
-                    "value": payload.insert_user,
-                    "short": true
-                },
-                {
-                    "title": "更新者",
-                    "value": payload.update_user,
-                    "short": true
-                }
-            ],
-            "color": ((payload.status === 'created') ? '#7CD197' : '#F35A00')
-        }
-    ];
+    if (!payload.public_flag) {
+        // Slack への投稿フォーマット指定
+        // cf. https://api.slack.com/incoming-webhooks
+        response.attachments = [
+            {
+                "pretext": process.env.KNOWLEDGE_MAN + 'の悪知恵が' + ((payload.status === 'created') ? '増えました' : '更新されました'),
+                "title": payload.title,
+                "title_link": payload.link,
+                "text": payload.content,
+                "fields": [
+                    {
+                        "title": "登録者",
+                        "value": payload.insert_user,
+                        "short": true
+                    },
+                    {
+                        "title": "更新者",
+                        "value": payload.update_user,
+                        "short": true
+                    }
+                ],
+                "color": ((payload.status === 'created') ? '#7CD197' : '#F35A00')
+            }
+        ];
 
-    proxy(JSON.stringify(response));
+        proxy(JSON.stringify(response));
+    }
 
     response.attachments = [];
     res.send('ok');
@@ -84,35 +87,37 @@ app.post('/comment', function (req, res) {
     console.log('event: ' + JSON.stringify(req.body, null, 4));
     var payload = req.body;
 
-    // Slack への投稿フォーマット指定
-    // cf. https://api.slack.com/incoming-webhooks
-    response.attachments = [
-        {
-            "pretext": process.env.KNOWLEDGE_MAN + 'の悪知恵にコメントが追加されました',
-            "title": payload.knowledge.title,
-            "title_link": payload.knowledge.link,
-            "text": payload.comment,
-            "fields": [
-                {
-                    "title": "登録者",
-                    "value": payload.insert_user,
-                    "short": true
-                },
-                {
-                    "title": "更新者",
-                    "value": payload.update_user,
-                    "short": true
-                },
-                {
-                    "title": "記事",
-                    "value": payload.knowledge.content
-                }
-            ],
-            "color": "#764FA5"
-        }
-    ];
+    if (!payload.knowledge.public_flag) {
+        // Slack への投稿フォーマット指定
+        // cf. https://api.slack.com/incoming-webhooks
+        response.attachments = [
+            {
+                "pretext": process.env.KNOWLEDGE_MAN + 'の悪知恵にコメントが追加されました',
+                "title": payload.knowledge.title,
+                "title_link": payload.knowledge.link,
+                "text": payload.comment,
+                "fields": [
+                    {
+                        "title": "登録者",
+                        "value": payload.insert_user,
+                        "short": true
+                    },
+                    {
+                        "title": "更新者",
+                        "value": payload.update_user,
+                        "short": true
+                    },
+                    {
+                        "title": "記事",
+                        "value": payload.knowledge.content
+                    }
+                ],
+                "color": "#764FA5"
+            }
+        ];
 
-    proxy(JSON.stringify(response));
+        proxy(JSON.stringify(response));
+    }
 
     response.attachments = [];
     res.send('ok');
